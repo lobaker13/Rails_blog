27f884f70b724a6bb6c1d682174741ed45afed91
Logan Baker <lobaker13@gmail.com>
2017-07-16 19:16:13 -0400

    Some more of that functionality stuff

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
-    @comment = Comment.new
+    @comment = Comment.new( post_id: params[:post_id] )
   end
 
   # GET /comments/1/edit
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:body, :user_id, :post)
+      params.require(:comment).permit(:body, :post_id)
     end
 end
